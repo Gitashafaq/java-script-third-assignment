dd6cbea47f882fbcaeecb87025e4cd15eb2a4eaf
Gita Shafaq <gitashafaq2613@gmail.com>
2024-03-04 23:20:53 +0430

    Add files via upload

---
+++ 3 javascript assignment/app.js
@@ -0,0 +1,7 @@
+let element=document.getElementsByClassName("header")[0];
+function showMenu(){
+    element.setAttribute("class","hhhh");
+}
+function hideMenu(){
+    element.setAttribute("class", "hide");
+}
